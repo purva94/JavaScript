0f484c9da9f032027c5ac6476449a6d88cd28e1b
Purva shrivastva <shpurva6@gmail.com>
2025-09-19 11:28:47 +0530

    Math object

---
+++ BAsic/Math.js
@@ -0,0 +1,38 @@
+// Math is an object that provides properties
+// (like PI) and methods (like round(), sqrt(), etc.) for mathematical operations.
+
+let x = 4.8;
+let y = 6;
+let z = 9;
+
+let maxi = Math.max(x, y, z);
+console.log(maxi);
+let min = Math.min(x, x, z);
+console.log(min);
+
+let p = Math.round(x);
+console.log(p);
+
+// Short Notes for Revision:
+
+// round() → nearest integer
+
+// floor() → down
+
+// ceil() → up
+
+// trunc() → remove decimal
+
+// pow(x,y) → x^y
+
+// sqrt() → square root
+
+// log(x) → natural log
+
+// sin/cos/tan(x) → trig (in radians)
+
+// abs(x) → positive value
+
+// sign(x) → returns -1, 0, 1
+
+// max/min() → biggest / smallest
