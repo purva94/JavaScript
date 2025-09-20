08065345017f08495851662c2341460487eb70e9
Purva shrivastva <shpurva6@gmail.com>
2025-09-20 14:50:30 +0530

    Ternary Operator

---
+++ BAsic/Ternary_op.js
@@ -0,0 +1,19 @@
+// Ternary operator is a shortcut of if else statement
+// condition ? codeifTrue : codeiffalse
+
+// let age = 10;
+// let message = age >= 19 ? "you are an adult " : "yoou are a minor";
+// console.log(message);
+
+// let time = 9;
+// let dinner_time =
+//   time >= 6 ? "well it's a dinner time" : "no it's not a dinner time";
+// console.log(dinner_time);
+
+// let time = 11;
+// let greeting = time <= 12 ? "Good morning" : "Good afternoon";
+// console.log(greeting);
+
+let isStudent = true;
+let message = isStudent ? "You are a student " : "you are not a student ";
+console.log(message);
