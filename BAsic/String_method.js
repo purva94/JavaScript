04772a8fc63d04d1b07054df19672dca9ba06b98
Purva shrivastva <shpurva6@gmail.com>
2025-09-20 15:30:18 +0530

    String methods

---
+++ BAsic/String_method.js
@@ -0,0 +1,14 @@
+let username = "   purvaa   ";
+
+console.log(username.charAt(0));
+console.log(username.indexOf("v"));
+console.log(username.lastIndexOf("a"));
+console.log(username.length);
+console.log(username.trim());
+console.log(username.toUpperCase());
+console.log(username.toLowerCase());
+
+let phone = "626-890-9914";
+console.log(phone.replaceAll("-", "/"));
+console.log(phone.padStart("45", "*"));
+console.log(phone.padEnd("45", "*"));
