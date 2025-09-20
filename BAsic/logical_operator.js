cea602f4b0c5f1fd72a944139f569b26bdf27c1f
Purva shrivastva <shpurva6@gmail.com>
2025-09-20 19:45:27 +0530

    logical operator

---
+++ BAsic/logical_operator.js
@@ -0,0 +1,37 @@
+// logical operator -- is used to combine or manipulate Boolean values (true/false)
+
+//And --> &&
+//or -->  ||
+//not -->  !
+
+//And operator
+/*let temp = 100;
+
+if (temp >= 90 && temp > 50) {
+  in the and logical operator the both condition must be true
+  console.log("the weather is good");
+} else {
+  console.log("the weather is not good");
+}
+  */
+
+//Or logical operator
+
+/*const a = 90;
+const b = 80; 
+if (a > 100 || b > 70) {   //if only one condition is ture 
+  console.log("you are pass broo chill !");
+} else {
+  console.log("you are fail");
+}
+*/
+
+//Not operator
+// not operator changes the true to false and false to ture
+
+const issunny = true;
+if (!issunny) {
+  console.log("It's sunny");
+} else {
+  console.log("it's cloudy");
+}
