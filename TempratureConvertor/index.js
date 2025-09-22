33e8f3fbb471012cf5eb154968f513d7ffa109e8
Purva shrivastva <shpurva6@gmail.com>
2025-09-22 17:30:21 +0530

    TempratureConvertor

---
+++ TempratureConvertor/index.js
@@ -0,0 +1,18 @@
+const text = document.getElementById("text");
+const toFehrenhiet = document.getElementById("toFehrenhiet");
+const tocelsius = document.getElementById("tocelsius");
+const result = document.getElementById("result");
+let temp;
+function convert() {
+  if (toFehrenhiet.checked) {
+    temp = Number(text.value);
+    temp = (temp * 9) / 5 + 32;
+    result.textContent = temp.toFixed(1) + "°F";
+  } else if (tocelsius.checked) {
+    temp = Number(text.value);
+    temp = (temp - 32) * (5 / 9);
+    result.textContent = temp.toFixed(1) + "°F";
+  } else {
+    result.textContent = "Please select the unit";
+  }
+}
