dfb86e652190c8b21b333ee6e4be27a5ac64736f
Purva shrivastva <shpurva6@gmail.com>
2025-09-22 16:10:12 +0530

    Varable scope

--- BAsic/Variable_scope.js
+++ BAsic/Variable_scope.js
@@ -1,9 +1,9 @@
 // Variable Scope= where a variable is recognized
 // and accessible (global Vs local)
 
-let b = 18; //global scope
+let b = 18; //global scope can be accessible throught the program
 function f1() {
-  let a = 9; //local scope
+  let a = 9; //local scope only accesible inside the function
   console.log(a);
   console.log(b);
 }
